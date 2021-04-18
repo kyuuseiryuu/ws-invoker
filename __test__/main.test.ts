c74fd9cdf59d771df6c9e00e06f0c679ccd55f9a
kyuuseiryuu <kyuuseiryuu@gmail.com>
2021-04-18 17:25:34 +0900

    refactory

--- __test__/main.test.ts
+++ __test__/main.test.ts
@@ -1,7 +1,32 @@
-const Invoker = require('../');
+import Invoker from '../dist';
+import {w3cwebsocket} from "websocket";
 
-const invoker = new Invoker({});
+const invoker = new Invoker();
 
 describe('main test', () => {
-  invoker.invoke()
+  test('test', async (done) => {
+    const ws = new w3cwebsocket('ws://127.0.0.1:3030/ws');
+    invoker.setWebSocket(ws as any);
+    ws.onopen = () => {
+      invoker.invoke('add', {a: 1, b: -2}, result => {
+        expect(result).toBe(-1);
+        ws.close();
+        done();
+      });
+    }
+  });
+  test('test no implemented function', async (done) => {
+    const ws = new w3cwebsocket('ws://127.0.0.1:3030/ws');
+    invoker.setWebSocket(ws as any);
+    ws.onopen = () => {
+      invoker.invoke('test', null, () => {
+        console.log('test');
+        done();
+      }, error => {
+        expect(typeof error.message).toBe('string');
+        ws.close();
+        done();
+      });
+    }
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,15 @@ interface IOption {
   logger: any;
 }
 
+interface IError {
+  message: string;
+  error: any;
+}
+
+interface IDoneFunc<T> {
+  (result: T): void;
+}
+
 interface IFuncMap {
   [funcName: string]: (params?: any) => any;
 }
@@ -12,6 +21,8 @@ interface IWebSocketInvokeRequest<T> {
   param?: T;
 }
 
+type ImplementReturnType<T> = T | Promise<T> | void | any;
+
 const defaultCallback = () => {
   // nothing;
 };
@@ -19,20 +30,32 @@ const defaultOnError = () => {
   // nothing;
 };
 
+interface IInvokeCallback<T> {
+  (params: T): ImplementReturnType<T>;
+}
+
+interface IOnErrorCallback {
+  (error: IError): any;
+}
+
 export default class Invoker {
 
   private static callbackFuncName (funcName: string, type: "DONE" | "ERROR" = "DONE"): string {
     return [funcName, type].join("_");
   }
 
-  private ws: WebSocket;
+  private ws?: WebSocket;
   private readonly funcMap: IFuncMap;
+  private readonly doneFuncMap: IFuncMap;
+  private readonly errorFuncMap: IFuncMap;
   private readonly option?: IOption;
 
-  public constructor (websocket: WebSocket, option?: IOption) {
+  public constructor (websocket?: WebSocket, option?: IOption) {
     this.option = option;
     this.ws = websocket;
     this.funcMap = {};
+    this.doneFuncMap = {};
+    this.errorFuncMap = {};
     this.setupWebSocket();
   }
 
@@ -41,7 +64,8 @@ export default class Invoker {
     this.setupWebSocket();
   }
 
-  public implement<T, R> (funcName: string, implementFunc?: (param?: T) => void | Promise<R>) {
+
+  public implement<P = any, R = any> (funcName: string, implementFunc?: (param?: P) => ImplementReturnType<R>) {
     if (!funcName || !implementFunc) {
       return;
     }
@@ -51,54 +75,77 @@ export default class Invoker {
     }
   }
 
-  public invoke<T, R> (
+  public invoke<P = any, R = any> (
     funcName: string,
-    param?: T,
-    callback?: (param?: R) => void | Promise<R>,
-    onError?: (error?: string) => void,
+    param?: P,
+    callback?: IInvokeCallback<R>,
+    onError?: IOnErrorCallback,
   ) {
     this.option?.logger.log("Call remote method", { funcName });
     if (Boolean(callback)) {
-      this.implement(Invoker.callbackFuncName(funcName), callback);
+      this.implementDoneFunc<R>(funcName, callback);
     }
     if (Boolean(onError)) {
-      this.implement(Invoker.callbackFuncName(funcName, "ERROR"), onError);
+      this.implementErrorFunc(funcName, onError);
     }
-    this.ws.send(JSON.stringify({
+    this.ws?.send(JSON.stringify({
       funcName,
       param,
-    } as IWebSocketInvokeRequest<T>));
+    } as IWebSocketInvokeRequest<P>));
   }
 
   private setupWebSocket () {
+    if (!this.ws) return;
     this.ws.onmessage = async (ev) => {
       await this.onMessage(ev.data.toString());
     };
+    Object.keys(this.funcMap).forEach(k => {
+      this.implement(k, this.funcMap[k]);
+    });
+    Object.keys(this.doneFuncMap).forEach(k => {
+      this.implementDoneFunc(k, this.doneFuncMap[k]);
+    });
+    Object.keys(this.errorFuncMap).forEach(k => {
+      this.implementErrorFunc(k, this.errorFuncMap[k]);
+    });
   }
 
   private async onMessage (message: string) {
     try {
       const { funcName, param } = JSON.parse(message) as IWebSocketInvokeRequest<any>;
-      if (!this.funcMap[funcName]) {
-        if (/_ERROR$/.test(funcName)) {
-          return;
-        }
-        this.option?.logger.log("Method not implement", { funcName });
-        this.invoke(Invoker.callbackFuncName(funcName, "ERROR"), { message: "Method not implement" });
+      const [realFuncName, funcType, ...overflow] = funcName.split('_');
+      if (!funcType && !this.funcMap[realFuncName]) {
+        this.invoke(Invoker.callbackFuncName(realFuncName, "ERROR"), { message: "Method not implement" });
+        this.option?.logger.log("Method not implement", { funcName: realFuncName });
+        return;
+      }
+      if (funcType === 'ERROR' && this.errorFuncMap[realFuncName]) {
+        this.errorFuncMap[realFuncName](param);
+        return;
+      }
+      if (funcType === "DONE" && this.doneFuncMap[realFuncName]) {
+        this.doneFuncMap[realFuncName](param);
         return;
       }
       try {
-        const result = await this.funcMap[funcName](param);
-        if (/_DONE$/.test(funcName)) {
-          return;
-        }
-        this.invoke(Invoker.callbackFuncName(funcName), result);
+        const result = await this.funcMap[realFuncName](param);
+        this.invoke(Invoker.callbackFuncName(realFuncName, "DONE"), result);
       } catch (e) {
         this.option?.logger.log("Processing message error", { raw: message });
-        this.invoke(Invoker.callbackFuncName(funcName, "ERROR"), e);
+        this.invoke(Invoker.callbackFuncName(funcName, "ERROR"), { message: e.message, error: e });
       }
     } catch (error) {
       this.option?.logger.log("Parse message error", { error: error.message, message });
     }
+  }
+
+  private implementDoneFunc<T>(funcName: string, doneFunc?: IDoneFunc<T>) {
+    if (!funcName || !doneFunc) return;
+    this.doneFuncMap[funcName] = doneFunc;
+  }
+
+  private implementErrorFunc(funcName?: string, onError?: IOnErrorCallback) {
+    if (!funcName || !onError) return;
+    this.errorFuncMap[funcName] = onError;
   }
 }
